a99340950919b8a7957b6a924bbaba6ea3f3ea5c
SantiagoAV <42415733+SantiagoAV@users.noreply.github.com>
2019-05-20 18:33:22 -0500

    funciona con json

--- src/Inventario/product/urls.py
+++ src/Inventario/product/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
-    path('<int:codigoBarras>/detail/', views.buscar, name='detail'),
+    path('productos/<pk>/', views.darProducto, name='detail'),
     path('productos/', views.listarProductos),
     path('', views.index),
     path('productocreate/', views.registrar),
--- src/Inventario/product/views.py
+++ src/Inventario/product/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect, HttpResponse
-
+from django.http import JsonResponse
+from .models import product
+import json
 
 from .forms import ProductForm
 from .models import product
@@ -11,25 +13,33 @@ def buscar(request, codigoBarras):
     return HttpResponseRedirect("Detalles del producto %s" %codigoBarras)
 
 def registrar(request):
+    if request.method == 'POST':
+        data = request.body.decode('utf-8')
+        data_json = json.loads(data)
+        producto = product()
+        producto.codigoBarras = data_json["codigoBarras"]
+        producto.precio = data_json["precio"]
+        producto.unidades = data_json["unidades"]
+        producto.iva = data_json["iva"]
+        producto.nombre = data_json["nombre"]
+        producto.categoria = data_json["categoria"]
+        producto.save()
+        return HttpResponse("El registro del producto fue exitoso")
 
-
-        form = ProductForm(request.POST or None)
-        if form.is_valid():
-            form.save()
-            form = ProductForm()
-        context = {
-            'form': form,
-        }
-        return render(request, 'Post_producto.html', context)
 
 
 
 def listarProductos(request):
     queryset = product.objects.all()
-    context = {
-        'productos_list' : queryset
-    }
-    return render(request, 'List_producto.html', context)
+    context = list(queryset.values('codigoBarras', 'precio', 'unidades', 'iva', 'nombre', 'categoria'))
+    return JsonResponse(context, safe=False)
+
+def darProducto(request, pk = None):
+    if pk:
+        producto = product.objects.get(codigoBarras= pk)
+
+    return JsonResponse({'codigoBarras':  producto.codigoBarras, "precio": producto.precio, "unidades": producto.unidades, "iva": producto.iva, "nombre": producto.nombre, "categoria": producto.categoria})
+
 
 def index(request):
     return render(request, 'index.html')
