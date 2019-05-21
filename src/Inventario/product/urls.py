d7fb7c2c8105d78d4e1ec9922864d13c64aea334
SantiagoAV <42415733+SantiagoAV@users.noreply.github.com>
2019-05-20 19:00:33 -0500

    funciona post

--- src/Inventario/product/urls.py
+++ src/Inventario/product/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.conf.urls import url
+from django.views.decorators.csrf import csrf_exempt
 
 from . import views
 
@@ -7,5 +8,5 @@ urlpatterns = [
     path('productos/<pk>/', views.darProducto, name='detail'),
     path('productos/', views.listarProductos),
     path('', views.index),
-    path('productocreate/', views.registrar),
+    url(r'productocreate/$', csrf_exempt(views.registrar)),
 ]
